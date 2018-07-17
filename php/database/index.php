773d3dbee8fba9ce37c86a5002d271b9085653fd
Ahmedhaseeb <ahmedhaseeb123@gmail.com>
2018-07-17 11:04:42 +0500

    database

--- php/database/index.php
+++ php/database/index.php
@@ -9,25 +9,18 @@
 		$db = new PDO('mysql:host=localhost;dbname=erozgar_2018;charset=utf8mb4', 'root', '');
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-
-
-
-
-
 		
 		try {
-			
-
 			$id = $_GET['id'];
 			//$stmt = $db->prepare("SELECT * FROM students where id > ?");
 			//$stmt = $db->prepare("DELETE FROM students where id = ?");
 			
-			$stmt = $db->prepare("UPDATE students set name = 'abc' , rollno='123' where id = ?");
-			
-			$stmt->execute(array($id));
-
-			
-
+			$stmt = $db->prepare("UPDATE students set name = 'ahsdagsdhaj' , rollno='465489' where id = ? and rollno = ?");
+			$stmt->execute(
+				array(
+					$id
+				)
+			);
 
 			//$stmt = $db->query("SELECT * FROM students WHERE id > $id");
 			$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
